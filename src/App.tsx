fbd6a979d2155b7bfbcfac9e03b2461754457bbd
jonney2021 <yeminghuhym@gmail.com>
2023-09-01 16:54:37 -0400

    setup the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React from "react";
-import "./App.css";
-import { HomePage } from "./layouts/HomePage/HomePage";
-import { Footer } from "./layouts/NavbarAndFooter/Footer";
-import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export const App = () => {
+function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <HomePage></HomePage>
-      <Footer></Footer>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
+
+export default App;
