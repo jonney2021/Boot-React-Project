44f990487b96432f83e8756f4d8cc1e9eb2c3a8e
jonney2021 <yeminghuhym@gmail.com>
2023-09-01 22:03:49 -0400

    added Navbar and ExploreTopBooks components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
 
 function App() {
+  // create a bootstrap navbar
   return (
-    <div className="App">
-      <h1>Welcome to the fullstack course!</h1>
+    <div>
+      <Navbar />
+      <ExploreTopBooks />
     </div>
   );
 }
