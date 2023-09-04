cd59584e57f907a6183950bc0b7938b540e8d136
jonney2021 <yeminghuhym@gmail.com>
2023-09-04 11:44:24 -0400

    ConsumeCarouselBookAPI

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,66 @@
 import { ReturnBook } from "./ReturnBook";
+import { useEffect, useState } from "react";
+import BookModel from "../../../models/BookModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const Carousel = () => {
+  // Create a book array
+  const [books, setBooks] = useState<BookModel[]>([]);
+  // For judging whether in loading
+  const [isLoading, setIsLoading] = useState(true);
+  // API call failure scenario
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const baseUrl: string = "http://localhost:8080/api/books";
+      const url: string = `${baseUrl}?page=0&size=9`;
+      // fetching the url data
+      const response = await fetch(url);
+      //failure scenario
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      // transfer the url data into json;
+      const responseJson = await response.json();
+      // get the data which is the object of embedded books
+      const responseData = responseJson._embedded.books;
+      // create a book array
+      const loadedBooks: BookModel[] = [];
+      // iterate the object in responseData, push them into the book array,and set the loading process finished.
+      for (const key in responseData) {
+        loadedBooks.push({
+          id: responseData[key].id,
+          title: responseData[key].title,
+          author: responseData[key].author,
+          description: responseData[key].description,
+          copies: responseData[key].copies,
+          copiesAvailable: responseData[key].copiesAvailable,
+          category: responseData[key].category,
+          img: responseData[key].img,
+        });
+      }
+      setBooks(loadedBooks);
+      setIsLoading(false);
+    };
+    fetchBooks().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container mt-5" style={{ height: 550 }}>
       <div className="homepage-carousel-title">
@@ -15,25 +75,25 @@ export const Carousel = () => {
         <div className="carousel-inner">
           <div className="carousel-item active">
             <div className="row d-flex justify-content-center align-item-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(0, 3).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
 
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-item-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(3, 6).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
 
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-item-center">
-              <ReturnBook />
-              <ReturnBook />
-              <ReturnBook />
+              {books.slice(6, 9).map((book) => (
+                <ReturnBook book={book} key={book.id} />
+              ))}
             </div>
           </div>
         </div>
@@ -65,7 +125,7 @@ export const Carousel = () => {
       {/* mobile */}
       <div className="d-lg-none mt-3">
         <div className="row d-flex justify-content-center align-items-center">
-          <ReturnBook />
+          <ReturnBook book={books[7]} key={books[7].id} />
         </div>
       </div>
       <div className="homepage-carousel-title mt-3">
