2c941ed897af49175e56889ecb8ac84786be7741
jonney2021 <yeminghuhym@gmail.com>
2023-09-10 14:25:26 -0400

    S14-ReviewsReact

--- src/layouts/BookCheckOutPage/BookCheckOutPage.tsx
+++ src/layouts/BookCheckOutPage/BookCheckOutPage.tsx
@@ -6,6 +6,7 @@ import { StarsReview } from "../Utils/StarsReview";
 import { CheckoutAndReviewBox } from "./CheckOutAndReviewBox";
 import { LatestReviews } from "./LatestReviews";
 import { useOktaAuth } from "@okta/okta-react";
+import ReviewRequestModel from "../../models/ReviewRequestModel";
 
 export const BookCheckOutPage = () => {
   const { authState } = useOktaAuth();
@@ -20,6 +21,9 @@ export const BookCheckOutPage = () => {
   const [totalStars, setTotalStars] = useState(0);
   const [isLoadingReview, setIsLoadingReview] = useState(true);
 
+  const [isReviewLeft, setIsReviewLeft] = useState(false);
+  const [isLoadingUserReview, setIsLoadingUserReview] = useState(true);
+
   // Loans Count State
   const [currentLoansCount, setCurrentLoansCount] = useState(0);
   const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] =
@@ -31,6 +35,7 @@ export const BookCheckOutPage = () => {
 
   const bookId = window.location.pathname.split("/")[2];
 
+  // fetch book by ID useEffect
   useEffect(() => {
     const fetchBook = async () => {
       const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
@@ -102,8 +107,40 @@ export const BookCheckOutPage = () => {
       setIsLoadingReview(false);
       setHttpError(error.message);
     });
-  }, []);
+  }, [isReviewLeft]);
 
+  // get a single review useEffect
+  useEffect(() => {
+    const fetchUserReviewBook = async () => {
+      //call api with the url and method type, headers
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/reviews/secure/user/book/?bookId=${bookId}`;
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        };
+        //fetch data with url and requestOptions
+        const userReview = await fetch(url, requestOptions);
+        //if failed
+        if (!userReview.ok) {
+          throw new Error("Somthing went wrong!");
+        }
+        // transfer to Json object
+        const userReviewResponseJson = await userReview.json();
+        setIsReviewLeft(userReviewResponseJson);
+      }
+      setIsLoadingUserReview(false);
+    };
+    fetchUserReviewBook().catch((error: any) => {
+      setIsLoadingUserReview(false);
+      setHttpError(error.message);
+    });
+  }, [authState]);
+
+  // loans count useEffect
   useEffect(() => {
     const fetchUserCurrentLoansCount = async () => {
       // make sure user is autenticated
@@ -171,7 +208,8 @@ export const BookCheckOutPage = () => {
     isLoading ||
     isLoadingReview ||
     isLoadingCurrentLoansCount ||
-    isLoadingBookCheckedOut
+    isLoadingBookCheckedOut ||
+    isLoadingUserReview
   ) {
     return <SpinnerLoading />;
   }
@@ -185,7 +223,9 @@ export const BookCheckOutPage = () => {
   }
 
   async function checkoutBook() {
+    // call the api
     const url = `http://localhost:8080/api/books/secure/checkout/?bookId=${book?.id}`;
+    // specify the method type and th headers
     const requestOptions = {
       method: "PUT",
       headers: {
@@ -193,11 +233,46 @@ export const BookCheckOutPage = () => {
         "Content-Type": "application/json",
       },
     };
+    // fetch the data
     const checkoutResponse = await fetch(url, requestOptions);
     if (!checkoutResponse.ok) {
       throw new Error("Something went wrong");
     }
     setIsCheckedOut(true);
+  }
+
+  // submit review
+  async function submitReview(starInput: number, reviewDescription: string) {
+    let bookId: number = 0;
+    if (book?.id) {
+      //grab the bookId
+      bookId = book.id;
+    }
+
+    //create a new review request based on the star input , bookId and description
+    const reviewRequestModel = new ReviewRequestModel(
+      starInput,
+      bookId,
+      reviewDescription
+    );
+    // url to access to our spring boot application
+    const url = `http://localhost:8080/api/reviews/secure`;
+    // method type and headers
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+        "Content-Type": "application/json",
+      },
+      //body for post method
+      body: JSON.stringify(reviewRequestModel),
+    };
+    //call the APi to fetch the data
+    const returnResponse = await fetch(url, requestOptions);
+    if (!returnResponse.ok) {
+      throw new Error("Something went wrong!");
+    }
+    setIsReviewLeft(true);
   }
 
   return (
@@ -234,6 +309,8 @@ export const BookCheckOutPage = () => {
             isAuthenticated={authState?.isAuthenticated}
             isCheckedOut={isCheckedOut}
             checkoutBook={checkoutBook}
+            isReviewLeft={isReviewLeft}
+            submitReview={submitReview}
           />
         </div>
         <hr />
@@ -271,6 +348,8 @@ export const BookCheckOutPage = () => {
           isAuthenticated={authState?.isAuthenticated}
           isCheckedOut={isCheckedOut}
           checkoutBook={checkoutBook}
+          isReviewLeft={isReviewLeft}
+          submitReview={submitReview}
         />
         <hr />
         <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
